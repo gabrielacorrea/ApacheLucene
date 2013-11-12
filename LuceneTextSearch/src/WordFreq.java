24378e01137615be780c0bac0e3680d82a368730
gabrielacorrea <gabriela01sc@gmail.com>
2013-11-11 23:54:50 -0200

    Project on Git

---
+++ LuceneTextSearch/src/WordFreq.java
@@ -0,0 +1,15 @@
+import org.apache.lucene.util.BytesRef;
+
+public class WordFreq {
+	public long count;
+	public BytesRef term;
+
+	public WordFreq(BytesRef term, long count) {
+		this.term = term;
+		this.count = count;
+	}
+
+	public WordFreq(String term, long count) {
+		this(new BytesRef(term), count);
+	}
+}
